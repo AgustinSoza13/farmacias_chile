762d64b6cd2f94e1b7a251777b37bfcbfc5ee0de
italo.pereda@galilea.cl <italo.pereda@galilea.cl>
2026-01-06 17:40:58 -0300

    Eliminacion basura, no se creara la base de datos en archivo

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI,HTTPException
-from fastapi.concurrency import asynccontextmanager
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import uvicorn
 import cloudscraper
-from sqlmodel import delete
 import os
-from sqlmodel import SQLModel, Field, Session, create_engine, select
+from sqlmodel import SQLModel, Field
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import APIKeyHeader
-API_KEY = "mi_clave_secreta_123"
-API_KEY_NAME = "X-API-KEY"
+API_KEY = os.getenv("mi_clave_secreta_123")
+API_KEY_NAME = os.getenv("API_KEY_NAME")
 
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
 app = FastAPI()
 
 def validar_api_key(api_key: str = Depends(api_key_header)):
@@ -21,17 +19,13 @@ def validar_api_key(api_key: str = Depends(api_key_header)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="API Key inválida"
         )
+
 @app.get("/farmacias_protegido")
 def farmacias_protegido(dep=Depends(validar_api_key)):
     return {"mensaje": "Acceso permitido"}
 
-API_1 = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php" # locales de turno
-API_2 = "https://midas.minsal.cl/farmacia_v2/WS/getLocales.php" #locales existentes
-
-sqlite_file_name = "farmacias.db"
-
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-engine = create_engine(sqlite_url)
+API_1 = os.getenv("API_1") # locales de turno
+API_2 = os.getenv("API_2") #locales existentes
 
 # Creamos el scraper que simula un navegador (Chrome en este caso)
 def scraper_minsal(API):
@@ -45,7 +39,6 @@ def scraper_minsal(API):
     return scraper.get(API,headers=headers, timeout=15)"""
     scraper = cloudscraper.create_scraper()
     headers = {
-        # User-Agent de un iPhone real
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
         "Accept": "application/json",
         "Accept-Language": "es-CL,es;q=0.9",
@@ -82,16 +75,13 @@ def extraccion_data_relevante(data):
 def get_fuente_uno(comuna: str = None, localidad: str = None):
     try:
         resp = scraper_minsal(API_2)
+        # Hacemos la petición
         if resp.status_code==403:
             for i in range(0,3):
                 resp = scraper_minsal(API_2)
                 if resp.status_code==200:
                     break
         if resp.status_code != 200:
-            #with Session(engine) as session:
-            #    farmacias = session.exec(select(Farmacia)).all()
-            #    data = [f.model_dump() for f in farmacias]
-                #TODO revisar en algun momento para los casos que este el documento
             print("STATUS:", resp.status_code)
             print("HEADERS:", resp.headers)
             print("BODY:", resp.text[:500])
@@ -119,8 +109,8 @@ def get_fuente_uno(comuna: str = None, localidad: str = None):
 @app.get("/getLocalesDeTurno")
 def get_fuente_dos(comuna: str = None, localidad: str = None):
     try:
-        # Hacemos la petición
         resp = scraper_minsal(API_1)
+        # Hacemos la petición
         if resp.status_code==403:
             for i in range(0,3):
                 print("intento :",i)
@@ -128,7 +118,6 @@ def get_fuente_dos(comuna: str = None, localidad: str = None):
                 if resp.status_code==200:
                     break
         if resp.status_code != 200:
-            # Si sigue dando 403, Cloudflare detectó el entorno de servidor
             print("STATUS:", resp.status_code)
             print("HEADERS:", resp.headers)
             print("BODY:", resp.text[:500])
@@ -148,54 +137,7 @@ def get_fuente_dos(comuna: str = None, localidad: str = None):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
-    
 
-
-def tarea_actualizar_farmacias():
-    """Función que ejecuta el cron para actualizar la DB"""
-    print("Iniciando actualización diaria de farmacias...")
-    data_raw = scraper_minsal(API_2)
-    
-    if not data_raw:
-        print("No se pudo obtener data del Minsal")
-        return
-    with Session(engine) as session:
-        # Opcional: Limpiar tabla antes de actualizar para no duplicar
-        
-        session.exec(delete(Farmacia))
-        session.commit()
-
-        for d in data_raw:
-            nueva_farmacia = Farmacia(
-                nombre=d.get("local_nombre"),
-                direccion=d.get("local_direccion"),
-                comuna=d.get("comuna_nombre"),
-                local_lat=d.get("local_lat"),
-                local_lng=d.get("local_lng")
-            )
-            session.add(nueva_farmacia)
-        
-        session.commit()
-        print(f"Éxito: {len(data_raw)} farmacias almacenadas.")
-
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Crear tablas si no existen
-    SQLModel.metadata.create_all(engine)
-    
-    # Configurar Cron
-    scheduler = AsyncIOScheduler()
-    # Todos los días a la 1:00 AM
-    #scheduler.add_job(tarea_actualizar_farmacias, "cron", hour=1, minute=0)
-    scheduler.add_job(tarea_actualizar_farmacias, "interval", seconds=2)
-    scheduler.start()
-    
-    yield
-    scheduler.shutdown()
-
-#app = FastAPI(lifespan=lifespan)
 if __name__ == "__main__":
     port = int(os.getenv("PORT",8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
